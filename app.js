a7e76deb48a98ac65d0f0d17e8bc260d6265d7a0
yetihead <yetibrain@yandex.ru>
2017-09-26 20:13:18 +0300

    next variant

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+'use strict';
+var arr = [4,3,'two','one'];
 
-const timeoutConsole = (arr) => {
-	const index = 0;
-
-	if (arr.length === 0) return;
-	return new Promise((res, rej) => {
+const displayValue = (value) => {
+	return new Promise(resolve => {
 		setTimeout(() => {
-			console.log( arr[0] )
-			res( arr.slice(1) );
-		}, 500)
-	}).then( timeoutConsole );
+			console.log('value is', value);
+			resolve();
+		}, 1000);
+	});
 };
 
+const start = (arr) => {
+	var index = 0;
+	const goOn = () => {
+		if (index < arr.length)
+			return displayValue( arr[index++] ).then( goOn );
+	};
+	return goOn(0)
+}
 
-timeoutConsole( [1,2,3,4] ).catch();
+start(arr);
